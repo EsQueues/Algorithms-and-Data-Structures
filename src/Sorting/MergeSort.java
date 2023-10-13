56f205e58b39eed8a17868bfb6a6f27934d88f38
Qusain Sayat <nomad1465QS@gmail.com>
2023-10-13 18:55:38 +0600

    Experimental commit

--- src/Sorting/MergeSort.java
+++ src/Sorting/MergeSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MergeSort {
     public static void main(String[] args) {
 
-        int []arr={8,2,1,4,9,7};
+        int []arr={8,2,1,4,9,23,42,12,32};
         mergeSort(arr);
         System.out.println(Arrays.toString(arr));
 
